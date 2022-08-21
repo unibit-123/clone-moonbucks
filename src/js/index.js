8409f946b1bf7c4a07e1970999fd153a2da73b42
PARK JAECHANG <73901174+unibit-123@users.noreply.github.com>
2022-08-22 00:48:43 +0900

    JS: update menu name

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
 function App() {
+
+    document.querySelector("#espresso-menu-list").addEventListener("click", (e) => {
+        if(e.target.classList.contains("menu-edit-button")) {
+            const $menuName = e.target.closest("li").querySelector(".menu-name")
+            const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+            $menuName.innerText = updatedMenuName;
+        }
+    });
+
     document.querySelector("#espresso-menu-form").addEventListener("submit", (e) => {
         e.preventDefault();
     });
