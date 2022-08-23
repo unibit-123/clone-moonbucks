bc56188da5bfdb395d288c2057040239d0fdfa8a
PARK JAECHANG <73901174+unibit-123@users.noreply.github.com>
2022-08-24 03:31:41 +0900

    JS: remove menu name, repactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,9 @@
 function App() {
 
-    document.querySelector("#espresso-menu-list").addEventListener("click", (e) => {
-        if(e.target.classList.contains("menu-edit-button")) {
-            const $menuName = e.target.closest("li").querySelector(".menu-name")
-            const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
-            $menuName.innerText = updatedMenuName;
-        }
-    });
-
-    document.querySelector("#espresso-menu-form").addEventListener("submit", (e) => {
-        e.preventDefault();
-    });
+    const updateMenuCount = () => {
+        const menuConut = document.querySelector("#espresso-menu-list").querySelectorAll("li").length
+        document.querySelector(".menu-count").innerText = `총 ${menuConut}개`;
+    };
 
     const addMenuName = () => {
         if(document.querySelector("#espresso-menu-name").value ==="") {
@@ -39,14 +32,38 @@ function App() {
         // 추가된 메뉴가 뒤쪽으로 추가됨
         document.querySelector("#espresso-menu-list").insertAdjacentHTML("beforeend", menuItemTemplate(espressoMenuName));
         // 총 개수 바꿔줌
-        const menuConut = document.querySelector("#espresso-menu-list").querySelectorAll("li").length
-        document.querySelector(".menu-count").innerText = `총 ${menuConut}개`;
+        updateMenuCount();
         document.querySelector("#espresso-menu-name").value = "";
     };
 
-    document.querySelector("#espresso-menu-submit-button").addEventListener("click", () => {
-        addMenuName();
+    const updateMenuName = (e) => {
+        const $menuName = e.target.closest("li").querySelector(".menu-name")
+        const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+        $menuName.innerText = updatedMenuName;
+    };
+
+    const removeMenuName = (e) => {
+        if(confirm("삭제하겠습니까 ?")) {
+            e.target.closest("li").remove();
+            updateMenuCount();
+        }
+    }
+
+    document.querySelector("#espresso-menu-list").addEventListener("click", (e) => {
+        if(e.target.classList.contains("menu-edit-button")) {
+            updateMenuName(e);
+        }
+
+        if (e.target.classList.contains("menu-remove-button")) {
+            removeMenuName(e);
+        }
     });
+
+    document.querySelector("#espresso-menu-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    document.querySelector("#espresso-menu-submit-button").addEventListener("click", addMenuName);
 
     document.querySelector("#espresso-menu-name").addEventListener("keypress", (e) => {
         if (e.key !=="Enter") {
